1599d6eaf8b0093beb4d1002cc6eb7b5028455e2
Andrew Luzin <andrew.luzin@gmail.com>
2022-08-01 15:55:01 +0700

    139 word break (#10)
    * Add BFS solution for 139 word break
    
    * Fix dir name

--- word-break/main.go
+++ word-break/main.go
@@ -10,10 +10,11 @@ func main() {
 	wordDict := []string{"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"}
 	s := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab" //nolint:lll
 
-	fmt.Println(wordBreak(s, wordDict))
+	fmt.Println(wordBreakBFS(s, wordDict))
 
 }
 
+//nolint:unused,deadcode
 func wordBreak(s string, wordDict []string) bool {
 	sort.Slice(wordDict, func(i, j int) bool {
 		return len(wordDict[i]) > len(wordDict[j])
@@ -22,6 +23,7 @@ func wordBreak(s string, wordDict []string) bool {
 	return wordBreakWithMemoization(s, wordDict, tried)
 }
 
+//nolint:unused
 func wordBreakWithMemoization(s string, wordDict []string, tried []bool) bool {
 	for i := range wordDict {
 		if tried[len(s)] {
@@ -37,5 +39,49 @@ func wordBreakWithMemoization(s string, wordDict []string, tried []bool) bool {
 		}
 	}
 	tried[len(s)] = true
+	return false
+}
+
+//nolint:gocognit
+func wordBreakBFS(s string, wordDict []string) bool {
+	wordMap := make(map[string]struct{}, len(wordDict))
+	wordsLegth := make([]bool, len(s)+1000)
+	maxWordLen := 0
+	for _, w := range wordDict {
+		wordMap[w] = struct{}{}
+		wordsLegth[len(w)] = true
+		if len(w) > maxWordLen {
+			maxWordLen = len(w)
+		}
+	}
+
+	visited := make([]bool, len(s))
+
+	stack := make([]int, 0, len(s))
+	stack = append(stack, 0)
+	for len(stack) > 0 {
+		curLength := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if visited[curLength] {
+			continue
+		}
+		for i := curLength + 1; i <= len(s); i++ {
+			wLen := i - curLength
+			if wLen > maxWordLen {
+				break
+			}
+			if !wordsLegth[wLen] {
+				continue
+			}
+			curStr := s[curLength:i]
+			if _, ok := wordMap[curStr]; ok {
+				if len(curStr)+curLength == len(s) {
+					return true
+				}
+				stack = append(stack, len(curStr)+curLength)
+			}
+		}
+		visited[curLength] = true
+	}
 	return false
 }
