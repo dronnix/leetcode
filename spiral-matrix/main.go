69c5330e3383974b2821412129acc28110cbd8f8
dronnix <andrew.luzin@gmail.com>
2022-08-19 22:57:26 +0400

    Add better simulation solution for 54: spiral matrix

--- spiral-matrix/main.go
+++ spiral-matrix/main.go
@@ -1,44 +1,29 @@
 package main
 
+// task: https://leetcode.com/problems/spiral-matrix/
+
 func spiralOrder(matrix [][]int) []int {
 	if len(matrix) == 0 {
 		return nil
 	}
-	resSize := len(matrix) * len(matrix[0])
-	result := make([]int, 0, resSize)
-	minI, minJ := 0, 0
-	maxI, maxJ := len(matrix), len(matrix[0])
 
-	for {
-		for j := minJ; j < maxJ; j++ {
-			result = append(result, matrix[minI][j])
+	cellsNum := len(matrix) * len(matrix[0])
+	result := make([]int, 0, cellsNum)
+
+	directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	steps := []int{len(matrix[0]), len(matrix) - 1, len(matrix[0]) - 1, len(matrix) - 2}
+	row, col := 0, -1
+	di := 0
+	for steps[di] > 0 {
+		for si := 0; si < steps[di]; si++ {
+			row += directions[di][0]
+			col += directions[di][1]
+			result = append(result, matrix[row][col])
 		}
-		if len(result) == resSize {
-			return result
-		}
-		minI++
-		for i := minI; i < maxI; i++ {
-			result = append(result, matrix[i][maxJ-1])
-		}
-		if len(result) == resSize {
-			return result
-		}
-		maxJ--
-		for j := maxJ - 1; j >= minJ; j-- {
-			result = append(result, matrix[maxI-1][j])
-		}
-		if len(result) == resSize {
-			return result
-		}
-		maxI--
-		for i := maxI - 1; i >= minI; i-- {
-			result = append(result, matrix[i][minJ])
-		}
-		if len(result) == resSize {
-			return result
-		}
-		minJ++
+		steps[di] -= 2
+		di = (di + 1) % len(directions)
 	}
+	return result
 }
 
 func main() {
