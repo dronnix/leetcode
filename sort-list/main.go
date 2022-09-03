e5cdaebe871fd5213ffbea5ed45110e98c681412
dronnix <andrew.luzin@gmail.com>
2022-09-03 12:25:53 +0400

    Add solution for 148: sort list (merge sort)

--- sort-list/main.go
+++ sort-list/main.go
@@ -10,20 +10,63 @@ type ListNode struct {
 }
 
 func sortList(head *ListNode) *ListNode {
-	result := ListNode{Val: -100001}
-	for head != nil {
-		ins := &result
-		for ins.Next != nil {
-			if head.Val < ins.Next.Val {
-				break
-			}
-			ins = ins.Next
-		}
-		h := head
-		head = head.Next
-		ins.Next, h.Next = h, ins.Next
+	if head == nil {
+		return nil
 	}
-	return result.Next
+	lists := divide(head)
+	return mergeAll(lists)
+}
+
+func divide(head *ListNode) []*ListNode {
+	result := make([]*ListNode, 0)
+	for head != nil {
+		result = append(result, head)
+		cur := head
+		head = head.Next
+		cur.Next = nil
+	}
+	if len(result)%2 > 0 {
+		result = append(result, nil)
+	}
+	return result
+}
+
+func mergeAll(lists []*ListNode) *ListNode {
+	for {
+		nl := make([]*ListNode, 0, len(lists)/2+1)
+		for i := 0; i < len(lists)-1; i += 2 {
+			nl = append(nl, merge(lists[i], lists[i+1]))
+		}
+		if len(nl) == 1 {
+			return nl[0]
+		}
+		if len(nl)%2 > 0 {
+			nl = append(nl, nil)
+		}
+		lists = nl
+	}
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+	fake := ListNode{}
+	ins := &fake
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			ins.Next = l1
+			l1 = l1.Next
+		} else {
+			ins.Next = l2
+			l2 = l2.Next
+		}
+		ins = ins.Next
+	}
+	if l1 != nil {
+		ins.Next = l1
+	}
+	if l2 != nil {
+		ins.Next = l2
+	}
+	return fake.Next
 }
 
 func main() {
