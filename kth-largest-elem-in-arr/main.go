d098397eae0f92a0bbc7d97d5d73a50f64761c92
dronnix <andrew.luzin@gmail.com>
2022-09-29 20:44:44 +0400

    Add solution for 215: kth largest element in an array

---
+++ kth-largest-elem-in-arr/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+// The task: https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, k int) int {
+	var hp IntHeap
+	hp = make([]int, 0, k)
+	h := &hp
+
+	for _, n := range nums {
+		heap.Push(h, n)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return heap.Pop(h).(int)
+}
+
+func main() {
+	fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))
+}
